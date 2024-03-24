871a7c69af7468931dea3043eebbcb77467d5b5f
yousefmohamed35 <yousefmo2021jo@gmail.com>
2024-03-25 00:11:41 +0200

    create splash view and its animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-import 'features/home.dart';
+import 'features/splash_view/presentation/views/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return const GetMaterialApp(
       home: MyHomePage(),
     );
   }
