12cccf68655e02beb7b91b30b4e5be09c3bfc461
yousefmohamed35 <yousefmo2021jo@gmail.com>
2024-03-24 23:37:03 +0200

    Add MyHomePage widget to home.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'features/home.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -15,20 +17,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Hello World!'),
-      ),
-    );
-  }
-}
