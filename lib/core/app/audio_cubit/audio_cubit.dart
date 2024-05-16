23599028e1f252064975ff9fb7b32ce5457d5bac
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-05-15 19:09:03 -0700

    create audio cubit and state

---
+++ lib/core/app/audio_cubit/audio_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:cv_project_team/core/app/audio_cubit/audio_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class AudioCubit extends Cubit<AudioState> {
+  AudioCubit() : super(AudioInitial());
+
+  
+}
---
+++ lib/core/app/audio_cubit/audio_state.dart
@@ -0,0 +1,13 @@
+class AudioState {}
+
+class AudioInitial extends AudioState{}
+
+class AudioPlaying extends AudioState{}
+
+class AudioPaused extends AudioState{}
+
+class AudioStopped extends AudioState{}
+
+class AudioLoading extends AudioState{}
+
+class AudioSeekTo extends AudioState{}
