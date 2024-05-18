8d981ffa4f3f0dd8bb1d55d2c4659151421afe89
yousefmohamed35 <yousefmo2021@gmail.com>
2024-05-18 12:45:54 -0700

    create select section page and finish page indicator

---
+++ lib/features/select_section/persentation/widgets/page_indicators.dart
@@ -0,0 +1,112 @@
+import 'package:cv_project_team/core/common/models/section_model.dart';
+import 'package:flutter/material.dart';
+
+
+class PageIndicators extends StatelessWidget {
+  const PageIndicators({
+    super.key,
+    required this.sectionSelectorNotifier,
+    required this.pageNotifier,
+  });
+
+  final ValueNotifier<int> sectionSelectorNotifier;
+  final ValueNotifier<double> pageNotifier;
+
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder<int>(
+      valueListenable: sectionSelectorNotifier,
+      builder: (_, value, child) => AnimatedOpacity(
+        opacity: value != -1 ? 0 : 1,
+        duration: value != -1
+            ? const Duration(milliseconds: 1)
+            : const Duration(milliseconds: 400),
+        child: child,
+      ),
+      child: ValueListenableBuilder<double>(
+        valueListenable: pageNotifier,
+        builder: (_, value, __) => Center(
+          child: PageViewIndicators(
+            length: SectionCardModels.fakeValues.length,
+            pageIndex: value,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class PageViewIndicators extends StatelessWidget {
+  const PageViewIndicators({
+    required this.length,
+    required this.pageIndex,
+    super.key,
+  });
+
+  final int length;
+  final double pageIndex;
+
+  @override
+  Widget build(BuildContext context) {
+    final index = pageIndex;
+    return SizedBox(
+      height: 12,
+      child: Stack(
+        clipBehavior: Clip.none,
+        alignment: Alignment.center,
+        children: [
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              for (int i = 0; i < length; i++) ...[
+                const _Dot(),
+                if (i < length - 1) const SizedBox(width: 16),
+              ],
+            ],
+          ),
+          Positioned(
+            left: (16 * index) + (6 * index),
+            child: const _BorderDot(),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class _BorderDot extends StatelessWidget {
+  const _BorderDot();
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 12,
+      height: 12,
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          border: Border.all(color: Colors.orange, width: 2),
+          color: Colors.purple,
+          shape: BoxShape.circle,
+        ),
+      ),
+    );
+  }
+}
+
+class _Dot extends StatelessWidget {
+  const _Dot();
+
+  @override
+  Widget build(BuildContext context) {
+    return const SizedBox(
+      width: 6,
+      height: 6,
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          color: Colors.black,
+          shape: BoxShape.circle,
+        ),
+      ),
+    );
+  }
+}
