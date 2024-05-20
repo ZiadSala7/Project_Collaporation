2ff8c166ad85f14ec01081f0b9887008376ca1d7
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-05-20 09:58:15 -0700

    add app audio for whole app

---
+++ lib/core/styles/audio/app_audio.dart
@@ -0,0 +1,17 @@
+class Assets {
+  Assets._();
+
+  static const String audiosAmod2Khoras = "audios/amod2_khoras audios.mp3";
+  static const String audiosAzraaAud = "audios/azraa_aud.mp3";
+  static const String audiosEastHenyaAudios = "audios/eastHenya_audios.mp3";
+  static const String audiosLastPartKhorasAudios = "audios/last_part_khoras audios.mp3";
+  static const String audiosMaaedaAud = "audios/maaeda_aud.mp3";
+  static const String audiosMdkhlAud = "audios/mdkhl_aud.mp3";
+  static const String audiosNklAud = "audios/nkl_aud.mp3";
+  static const String audiosNorthHenyaAudios = "audios/northHenya_audios.mp3";
+  static const String audiosShklAud = "audios/shkl_aud.mp3";
+  static const String audiosShnAud = "audios/shn_aud.mp3";
+  static const String audiosSouthHenyaAudios = "audios/southHenya_audios.mp3";
+  static const String audiosTest = "audios/test.mp3";
+}
+
