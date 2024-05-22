3110b912091bad43ecb71785546c796631087f78
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-05-22 14:52:55 -0700

    add colors

--- lib/core/styles/colors/app_color.dart
+++ lib/core/styles/colors/app_color.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const mainColor = Colors.brown;
+  static const mainColor = Color(0XFFFED9A1);
+  static const appWhite = Color(0XFFFFFFFF);
+  static const appBlack = Color(0XFF000000);
+  static const appGrey1 = Color(0XFFEEE8D2);
+  static const appGrey2 = Color(0XFFB8B89E);
+  static const appGrey3 = Color(0XFF839088);
   static const secondryColor = Color.fromARGB(255, 207, 191, 86);
   static const backgroundOfSection = Color.fromARGB(255, 174, 142, 0);
 }
