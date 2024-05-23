fbc6d944ae827762d53a1121740f2ff68397a2cb
yousefmohamed35 <yousefmo2021@gmail.com>
2024-05-22 22:31:23 -0700

    create audio screen

---
+++ lib/features/audios_screen/presentation/view/audio_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AudioScreen extends StatelessWidget {
+  const AudioScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
