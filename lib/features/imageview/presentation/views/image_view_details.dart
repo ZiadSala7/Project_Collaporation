e1ce87a4ef767723ec38625a980a63387d522c8c
yousefmohamed35 <yousefmo2021@gmail.com>
2024-05-30 00:15:56 -0700

    finsh image view details

--- lib/features/imageview/presentation/views/image_view_details.dart
+++ lib/features/imageview/presentation/views/image_view_details.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:photo_view/photo_view.dart';
 import 'package:photo_view/photo_view_gallery.dart';
 
@@ -21,18 +22,32 @@ class _ImageViewDetailsState extends State<ImageViewDetails> {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        PhotoViewGallery.builder(
-            itemCount: widget.images.length,
-            pageController: controller,
-            builder: (context, index) {
-              return PhotoViewGalleryPageOptions(
-                imageProvider: AssetImage(widget.images[index]),
-                initialScale: PhotoViewComputedScale.contained * .85,
-              );
-            })
-      ],
+    return SafeArea(
+      child: Stack(
+        children: [
+          PhotoViewGallery.builder(
+              itemCount: widget.images.length,
+              pageController: controller,
+              builder: (context, index) {
+                return PhotoViewGalleryPageOptions(
+                  imageProvider: AssetImage(widget.images[index]),
+                  initialScale: PhotoViewComputedScale.contained * .85,
+                );
+              }),
+          Padding(
+            padding: EdgeInsets.only(left: 16.w, top: 16.h),
+            child: IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: Icon(
+                  Icons.arrow_back_ios,
+                  color: Colors.white,
+                  size: 22.sp,
+                )),
+          )
+        ],
+      ),
     );
   }
 }
