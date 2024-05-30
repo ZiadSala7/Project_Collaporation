70793c328a3942e19ddf8b14009b7ebef70d1120
yousefmohamed35 <yousefmo2021@gmail.com>
2024-05-29 23:16:45 -0700

    some changes on padding in image text body

--- lib/features/section_details/presentation/widgets/image_text_body.dart
+++ lib/features/section_details/presentation/widgets/image_text_body.dart
@@ -1,6 +1,5 @@
 import 'package:cv_project_team/core/common/models/section_model.dart';
 import 'package:cv_project_team/core/common/widgets/custom_app_image.dart';
-import 'package:cv_project_team/core/language/app_localizations.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -10,18 +9,18 @@ class ImageTextBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final local = AppLocalizations.of(context);
-
     return Padding(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 10,
+        padding: EdgeInsets.symmetric(
+          horizontal: 16.w,
+          vertical: 15.h,
         ),
         child: ListView.builder(
             itemCount: section.images.length,
             itemBuilder: (context, index) {
               return Padding(
-                  padding: EdgeInsets.all(16.0.sp),
-                  child: CustomAppImage(imgPath: section.images[index]));
+                padding: EdgeInsets.only(bottom: 8.h),
+                child: CustomAppImage(imgPath: section.images[index]),
+              );
             }));
   }
 }
