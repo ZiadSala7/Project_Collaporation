9d34af6cd738b8c8159363cae820f82436255945
Mahmoud-Adel-Ali <mahmoudadela413@gmail.com>
2024-06-04 14:48:32 -0700

    chage grid view to list view in hikal_view

--- lib/features/section_details/presentation/view/hikal_view.dart
+++ lib/features/section_details/presentation/view/hikal_view.dart
@@ -7,43 +7,44 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 class HikalView extends StatelessWidget {
   const HikalView({super.key, required this.section});
   final SectionCardModels section;
+
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 16.h),
-      child: GridView.count(
-          crossAxisCount: 2,
-          mainAxisSpacing: 15.h,
-          crossAxisSpacing: 15.w,
-          childAspectRatio: .7.h,
-          children: [
-            CustomHikalSectionCard(
-              title: 'General',
-              imagePath: section.images[66],
-              onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => GeneralHikalView(
-                            images: section.images.sublist(67))));
-              },
-            ),
-            CustomHikalSectionCard(
-              title: 'North',
-              imagePath: section.images[42],
-              onTap: () {},
-            ),
-            CustomHikalSectionCard(
-              title: 'South',
-              imagePath: section.images[63],
-              onTap: () {},
-            ),
-            CustomHikalSectionCard(
-              title: 'East',
-              imagePath: section.images[17],
-              onTap: () {},
-            ),
-          ]),
+      child: ListView(
+        children: [
+          CustomHikalSectionCard(
+            title: 'General',
+            imagePath: section.images[66],
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => GeneralHikalView(
+                    images: section.images.sublist(67),
+                  ),
+                ),
+              );
+            },
+          ),
+          CustomHikalSectionCard(
+            title: 'North',
+            imagePath: section.images[42],
+            onTap: () {},
+          ),
+          CustomHikalSectionCard(
+            title: 'South',
+            imagePath: section.images[63],
+            onTap: () {},
+          ),
+          CustomHikalSectionCard(
+            title: 'East',
+            imagePath: section.images[17],
+            onTap: () {},
+          ),
+        ],
+      ),
     );
   }
 }
@@ -58,38 +59,41 @@ class CustomHikalSectionCard extends StatelessWidget {
   final void Function()? onTap;
   final String title;
   final String imagePath;
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
       child: Container(
-        padding: const EdgeInsets.all(6),
+        margin: EdgeInsets.symmetric(vertical: 20.h),
         decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(20),
-            border: Border.all(width: 4, color: Colors.brown),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.1),
-                spreadRadius: 1.w,
-                blurRadius: 1.w,
-                offset: Offset(0, 1.h),
-              )
-            ]),
+          borderRadius: BorderRadius.circular(20),
+          border: Border.all(width: 4, color: Colors.brown),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.1),
+              spreadRadius: 1.r,
+              blurRadius: 1.r,
+              offset: Offset(0, 1.h),
+            ),
+          ],
+        ),
         child: Column(
           children: [
-            Image.asset(
-              imagePath,
-              height: 145.h,
-              width: 150.w,
-              fit: BoxFit.fill,
+            AspectRatio(
+              aspectRatio: 1,
+              child: Image.asset(
+                imagePath,
+                fit: BoxFit.cover,
+                height: 80.h,
+                width: 80.w,
+              ),
             ),
-            SizedBox(
-              height: 10.h,
-            ),
+            SizedBox(height: 10.h),
             Text(
               title,
               style: Styless.textStyle14,
-            )
+            ),
           ],
         ),
       ),
