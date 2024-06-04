987c1bfe43e82f1e06f42ff141f8197e45c4c802
Ziad Salah <zslah1935@gmail.com>
2024-06-04 14:33:48 +0300

    import description

--- lib/features/section_details/presentation/widgets/details_view_body.dart
+++ lib/features/section_details/presentation/widgets/details_view_body.dart
@@ -3,6 +3,8 @@ import 'package:cv_project_team/core/language/app_localizations.dart';
 import 'package:cv_project_team/core/language/lang_keys.dart';
 import 'package:cv_project_team/core/styles/colors/app_color.dart';
 import 'package:cv_project_team/features/section_details/presentation/view/audio_screen.dart';
+import 'package:cv_project_team/features/section_details/presentation/view/description_screen.dart';
+import 'package:cv_project_team/features/section_details/presentation/view/hikal_view.dart';
 import 'package:cv_project_team/features/section_details/presentation/view/images_text.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
