9ffeaada16a143d6885b49af09376b69414ae763
Ziad Salah <zslah1935@gmail.com>
2024-06-04 20:37:10 +0300

    adding description

--- lib/features/section_details/presentation/view/description_screen.dart
+++ lib/features/section_details/presentation/view/description_screen.dart
@@ -1,4 +1,8 @@
+import 'package:cv_project_team/core/app/constant.dart';
+import 'package:cv_project_team/core/common/animations/animate_do.dart';
 import 'package:cv_project_team/core/common/models/section_model.dart';
+import 'package:cv_project_team/core/language/app_localizations.dart';
+import 'package:cv_project_team/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
 class DescriptionScreen extends StatelessWidget {
@@ -8,8 +12,24 @@ class DescriptionScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(section.descriptionScreenTitle),
+      body: CustomFadeInDown(
+        duration: animationDuration,
+        child: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Padding(
+              padding: const EdgeInsets.all(15),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    AppLocalizations.of(context)!
+                        .translate(section.descriptionScreenTitle)
+                        .toString(),
+                    style: Styless.textStyle20,
+                  ),
+                ],
+              )),
+        ),
       ),
     );
   }
